8e51c50c1fa658d2454bff98596f6d1d5f39de34
Wade H <wade@bitovi.com>
2023-02-15 15:35:16 -0500

    AOS2-90 implement rule and tests

--- tools/eslint-rules/rules/angular/host-listener-on-method.spec.ts
+++ tools/eslint-rules/rules/angular/host-listener-on-method.spec.ts
@@ -1,3 +1,4 @@
+import { convertAnnotatedSourceToFailureCase } from '@angular-eslint/utils';
 import { TSESLint } from '@typescript-eslint/utils';
 import { rule, RULE_NAME } from './host-listener-on-method';
 
@@ -6,6 +7,45 @@ const ruleTester = new TSESLint.RuleTester({
 });
 
 ruleTester.run(RULE_NAME, rule, {
-  valid: [`const example = true;`],
-  invalid: [],
+  valid: [
+    {
+      name: 'should be allowed on method defintions',
+      code: `
+      @Component()
+      class MyComponent {
+        @HostListener('click')
+        handleClick(): void { }
+      }`,
+    },
+    {
+      name: 'should be allowed on members assigned to arrow functions',
+      code: `
+      @Component()
+      class MyComponent {
+        @HostListener('click') clickHandler = () => {};
+      }`,
+    },
+  ],
+  invalid: [
+    {
+      name: 'should error when decorator is used on member with no value',
+      code: `
+      @Component()
+      class MyComponent {
+        @HostListener('click') clickHandler!: () => void;
+      }`,
+      errors: [{ messageId: 'hostListenerOnMethod' }],
+    },
+    convertAnnotatedSourceToFailureCase({
+      description: 'Should warn when decorator is used on non-function member',
+      annotatedSource: `
+@Component()
+class MyComponent {
+  @HostListener('click')
+  ~~~~~~~~~~~~~~~~~~~~~~
+  myProperty = true;
+}`,
+      messageId: 'hostListenerOnMethod',
+    }),
+  ],
 });
--- tools/eslint-rules/rules/angular/host-listener-on-method.ts
+++ tools/eslint-rules/rules/angular/host-listener-on-method.ts
@@ -14,7 +14,11 @@
  * https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin/src/rules
  */
 
-import { ESLintUtils } from '@typescript-eslint/utils';
+import {
+  ESLintUtils,
+  AST_NODE_TYPES,
+  TSESTree,
+} from '@typescript-eslint/utils';
 
 // NOTE: The rule will be available in ESLint configs as "@nrwl/nx/workspace/angular/host-listener-on-method"
 export const RULE_NAME = 'angular/host-listener-on-method';
@@ -24,14 +28,51 @@ export const rule = ESLintUtils.RuleCreator(() => __filename)({
   meta: {
     type: 'problem',
     docs: {
-      description: ``,
+      description: `@HostListener decorator must be used on a method or function member.`,
       recommended: 'error',
     },
     schema: [],
-    messages: {},
+    messages: {
+      hostListenerOnMethod: `@HostListener should decorate a method or a property with a method value.`,
+    },
   },
   defaultOptions: [],
   create(context) {
-    return {};
+    return {
+      [AST_NODE_TYPES.Decorator]: function (node: TSESTree.Decorator) {
+        const expression = node.expression;
+        if (
+          expression.type !== AST_NODE_TYPES.CallExpression ||
+          expression.callee.type !== AST_NODE_TYPES.Identifier
+        ) {
+          return;
+        }
+
+        const isHostListener = expression.callee?.name === 'HostListener';
+
+        if (!isHostListener) {
+          return;
+        }
+
+        const parentNode = node.parent;
+        if (
+          parentNode?.type === AST_NODE_TYPES.PropertyDefinition &&
+          (parentNode.value?.type === AST_NODE_TYPES.ArrowFunctionExpression ||
+            parentNode.value?.type === AST_NODE_TYPES.FunctionExpression)
+        ) {
+          // Decorator is on a property with a value that is an arrow function or a function expression, pass
+          return;
+        }
+        if (parentNode?.type === AST_NODE_TYPES.MethodDefinition) {
+          // Decorator is on a method, pass
+          return;
+        }
+
+        context.report({
+          node,
+          messageId: 'hostListenerOnMethod',
+        });
+      },
+    };
   },
 });
