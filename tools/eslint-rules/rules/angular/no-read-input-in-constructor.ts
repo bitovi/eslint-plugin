99673ce7acb9a8099bc58585d6cd116b96060ba2
Wade H <wade@bitovi.com>
2023-02-20 13:48:28 -0500

    AOS2-105 start building out rule

--- tools/eslint-rules/rules/angular/no-read-input-in-constructor.ts
+++ tools/eslint-rules/rules/angular/no-read-input-in-constructor.ts
@@ -14,7 +14,13 @@
  * https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin/src/rules
  */
 
-import { ESLintUtils } from '@typescript-eslint/utils';
+import {
+  ESLintUtils,
+  AST_NODE_TYPES,
+  TSESTree,
+} from '@typescript-eslint/utils';
+import { isInDecoratedClass } from '../../utilities/node/is-in-decorated-class';
+import { isCallableDecoratorWithName } from '../../utilities/left-land-side-expression/is-callable-decorator-with-name';
 
 // NOTE: The rule will be available in ESLint configs as "@nrwl/nx/workspace/angular/no-read-input-in-constructor"
 export const RULE_NAME = 'angular/no-read-input-in-constructor';
@@ -35,6 +41,45 @@ export const rule = ESLintUtils.RuleCreator(
   },
   defaultOptions: [],
   create(context) {
-    return {};
+    return {
+      [AST_NODE_TYPES.Decorator]: function (node: TSESTree.Decorator) {
+        if (!isCallableDecoratorWithName(node.expression, 'Input')) {
+          // Not @Input decorator, skip
+          return;
+        }
+
+        if (!isInDecoratedClass(node, ['Component', 'Directive'])) {
+          // Not in class decorated with @Component or @Directive, skip
+          return;
+        }
+
+        const classDeclaration = node.parent?.parent;
+        if (classDeclaration?.type !== AST_NODE_TYPES.ClassDeclaration) {
+          // Couldn't find class definition
+          return;
+        }
+
+        const constructor = classDeclaration.body.body.find(
+          (node) =>
+            node.type === AST_NODE_TYPES.MethodDefinition &&
+            node.key.type === AST_NODE_TYPES.Identifier &&
+            node.key.name === 'constructor'
+        );
+
+        if (
+          !constructor ||
+          constructor.type !== AST_NODE_TYPES.MethodDefinition ||
+          !constructor.value.body ||
+          constructor.value.body.body.length === 0
+        ) {
+          // No constructor, or empty constructor, skip
+          return;
+        }
+
+        // const constructorStatements = constructor.value.body.body;
+        // TODO: Look through statements for usage of input
+        // TODO: Consider changing to look at constructor FIRST
+      },
+    };
   },
 });
